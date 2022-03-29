f2341cb4b62cc7b8ce48e700dde74f95a00c0bf3
Eduard-lab-hub <eduardsavlu@gmail.com>
2022-03-29 13:24:30 +0600

    add css componets

--- project/src/App.js
+++ project/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './componets/Header';
+import Navbar from './componets/Navbar';
+import Profiles from './componets/Profiles';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profiles />
     </div>
   );
 }
---
+++ project/src/componets/Profiles.jsx
@@ -0,0 +1,27 @@
+import './Profiles.css';
+
+
+const Profiles = () =>{
+    return (
+        <div className='cont'>
+            <img src='https://tproger.ru/s3/uploads/2019/07/cover-Nachni-svoj-put-v-IT-880x308.png' alt='img'/>
+        <div>
+            ava + photo
+        </div>
+        <div>
+            My post
+            <div>
+                New post
+            </div>
+            <div>
+                post1
+            </div>
+            <div>
+                post2
+            </div>
+        </div>
+  </div>
+    );
+}
+
+export default Profiles;
